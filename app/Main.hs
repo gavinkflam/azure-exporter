b12a325d7d11e1d99156c9aa2c3005a08f0c05d3
Gavin Lam <me@gavin.hk>
2018-09-27 15:52:49 +0800

    Render gauges in exporter syntax

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,7 @@ import qualified Azure.Monitor.ListMetricValues as M
 import qualified Azure.OAuth2.AcquireAccessToken as T
 import qualified Azure.OAuth2.Data.AcquireAccessTokenResponse as TR
 import           AzureExporter.Monitor (gauges)
+import           AzureExporter.Text.Gauge (renderGauge)
 import           AzureExporterExe.Config
 import           AzureExporterExe.Control.Scotty (liftE)
 import           Control.Lens ((^.))
@@ -29,7 +30,7 @@ main = do
                             , M._timespan    = dummyTimespan
                             }
       metrics <- liftE $ liftIO $ M.listMetricValues token params
-      text $ L.pack $ show $ gauges metrics
+      text $ L.intercalate "\n" $ map renderGauge $ gauges metrics
 
 -- Dummy data
 dummyTimespan :: Text
