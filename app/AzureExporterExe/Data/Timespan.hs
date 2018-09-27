12032713869826b11e8d9c28a2e391cb7c8e763c
Gavin Lam <me@gavin.hk>
2018-09-27 17:45:18 +0800

    Added timespan functions

---
+++ app/AzureExporterExe/Data/Timespan.hs
@@ -0,0 +1,19 @@
+module AzureExporterExe.Data.Timespan
+  ( timespan
+  , getLastMinuteTimespan
+  ) where
+
+import Data.Time.Clock (UTCTime, addUTCTime, getCurrentTime)
+import Data.Time.Format (defaultTimeLocale, formatTime)
+
+timespan :: (UTCTime, UTCTime) -> String
+timespan (f, t) = iso8601UTCFormatTime f <> "/" <> iso8601UTCFormatTime t
+
+getLastMinuteTimespan :: IO String
+getLastMinuteTimespan = do
+  now <- getCurrentTime
+  return $ timespan (addUTCTime (-60) now, now)
+
+-- Formatting
+iso8601UTCFormatTime :: UTCTime -> String
+iso8601UTCFormatTime = formatTime defaultTimeLocale "%FT%H:%M:%SZ"
