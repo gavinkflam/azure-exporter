819c7376fa254f2910c34cd72b82e2a66ba78786
Gavin Lam <me@gavin.hk>
2018-09-27 11:30:21 +0800

    Added Maybe for type of metric values and changed Rational to Scientific

--- src/Azure/Monitor/Data/MetricValue.hs
+++ src/Azure/Monitor/Data/MetricValue.hs
@@ -15,7 +15,7 @@ module Azure.Monitor.Data.MetricValue
 import Azure.Monitor.Contract (aesonOptions)
 import Control.Lens (makeLenses)
 import Data.Aeson
-import Data.Ratio (Rational)
+import Data.Scientific (Scientific)
 import Data.Text (Text)
 import GHC.Generics
 import Prelude hiding (maximum, minimum)
@@ -23,12 +23,12 @@ import Prelude hiding (maximum, minimum)
 -- MetricValue
 -- https://docs.microsoft.com/en-us/rest/api/monitor/metrics/list#metricvalue
 data MetricValue =
-  MetricValue { _average   :: Rational
-              , _count     :: Int
-              , _maximum   :: Rational
-              , _minimum   :: Rational
+  MetricValue { _average   :: Maybe Scientific
+              , _count     :: Maybe Int
+              , _maximum   :: Maybe Scientific
+              , _minimum   :: Maybe Scientific
               , _timeStamp :: Text
-              , _total     :: Rational
+              , _total     :: Maybe Scientific
               } deriving (Generic, Show)
 
 instance FromJSON MetricValue where
