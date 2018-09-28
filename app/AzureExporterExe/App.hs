091f52b27926ae4041f9614e1351c624af248c68
Gavin Lam <me@gavin.hk>
2018-09-28 17:10:29 +0800

    Added request logger middleware

--- app/AzureExporterExe/App.hs
+++ app/AzureExporterExe/App.hs
@@ -4,11 +4,14 @@ module AzureExporterExe.App
   ( app
   ) where
 
-import           AzureExporterExe.Control.Monad.AppEnvReader
+import           AzureExporterExe.Control.Monad.AppEnvReader (AppEnvReader)
 import qualified AzureExporterExe.Route.Monitor as M
 import           Data.Text.Lazy (Text)
-import           Web.Scotty.Trans (ScottyT, get)
+import           Network.Wai.Middleware.RequestLogger (logStdout)
+import           Web.Scotty.Trans (ScottyT, get, middleware)
 
 app :: ScottyT Text AppEnvReader ()
 app = do
+  middleware logStdout
+  -- Monitor
   get "/monitor/metrics" M.metrics
