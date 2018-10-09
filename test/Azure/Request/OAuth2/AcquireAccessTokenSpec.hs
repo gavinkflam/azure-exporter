e87b0f66db5893cd7da4e6c6be63fa248a5350fd
Gavin Lam <me@gavin.hk>
2018-10-09 12:29:37 +0800

    Added unit tests for AcquireAccessToken request

---
+++ test/Azure/Request/OAuth2/AcquireAccessTokenSpec.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- |
+-- Test `Request` construction for the acquire access token API.
+module Azure.Request.OAuth2.AcquireAccessTokenSpec
+  (
+  -- * Spec
+    spec
+  ) where
+
+import           Azure.Request.OAuth2.AcquireAccessToken
+import qualified Data.ByteString.Char8 as C
+import           Data.Text.Lazy (unpack)
+import qualified DummyText as T
+import           Expectations
+import           Network.HTTP.Client (path, requestBody)
+import           Test.Hspec
+import           Util.HTTP (parseSimpleRequestBody)
+import           Util.Text (toBS)
+
+-- | Spec for `AcquireAccessToken`.
+spec :: Spec
+spec = do
+  let req    = request params
+      fItems = parseSimpleRequestBody $ requestBody req
+
+  describe "request" $ do
+    it "contains grant_type form item" $
+      fItems `shouldContain` [("grant_type", "client_credentials")]
+
+    it "contains resource form item" $
+      fItems `shouldContain` [("resource", "https://management.azure.com/")]
+
+    it "contains client_id form item" $
+      fItems `shouldContain` [("client_id", toBS T.clientId)]
+
+    it "contains client_secret form item" $
+      fItems `shouldContain` [("client_secret", toBS T.clientSecret)]
+
+    it "contains the expected path" $
+      C.unpack (path req) `shouldBe` expectedPath
+
+-- | Dummy `Params` item.
+params =
+  Params { _clientId     = T.clientId
+         , _clientSecret = T.clientSecret
+         , _tenantId     = T.tenantId
+         }
+
+-- |
+-- The expected path should
+--
+-- 1. Starts with the tenant ID
+-- 2. Follows by the API Endpoint
+expectedPath :: String
+expectedPath = unpack $ "/" <> T.tenantId <> "/oauth2/token"
--- test/DummyText.hs
+++ test/DummyText.hs
@@ -11,6 +11,9 @@ module DummyText
   , metricNames
   -- * OAuth 2.0
   , accessToken
+  , clientId
+  , clientSecret
+  , tenantId
   -- * Resource
   , resourceId
   -- * Timespan
@@ -51,6 +54,18 @@ metricNames = "Percentage CPU,Network In,Network Out"
 accessToken :: Text
 accessToken =
   "eyJhbGciOiJSUzI1NiIsIng1dCI6IjdkRC1nZWNOZ1gxWmY3R0xrT3ZwT0IyZGNWQSIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL3NlcnZpY2UuY29udG9zby5jb20vIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvN2ZlODE0NDctZGE1Ny00Mzg1LWJlY2ItNmRlNTdmMjE0NzdlLyIsImlhdCI6MTM4ODQ0ODI2NywibmJmIjoxMzg4NDQ4MjY3LCJleHAiOjEzODg0NTIxNjcsInZlciI6IjEuMCIsInRpZCI6IjdmZTgxNDQ3LWRhNTctNDM4NS1iZWNiLTZkZTU3ZjIxNDc3ZSIsIm9pZCI6ImE5OTE5MTYyLTkyMTctNDlkYS1hZTIyLWYxMTM3YzI1Y2RlYSIsInN1YiI6ImE5OTE5MTYyLTkyMTctNDlkYS1hZTIyLWYxMTM3YzI1Y2RlYSIsImlkcCI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0LzdmZTgxNDQ3LWRhNTctNDM4NS1iZWNiLTZkZTU3ZjIxNDc3ZS8iLCJhcHBpZCI6ImQxN2QxNWJjLWM1NzYtNDFlNS05MjdmLWRiNWYzMGRkNThmMSIsImFwcGlkYWNyIjoiMSJ9.aqtfJ7G37CpKV901Vm9sGiQhde0WMg6luYJR4wuNR2ffaQsVPPpKirM5rbc6o5CmW1OtmaAIdwDcL6i9ZT9ooIIicSRrjCYMYWHX08ip-tj-uWUihGztI02xKdWiycItpWiHxapQm0a8Ti1CWRjJghORC1B1-fah_yWx6Cjuf4QE8xJcu-ZHX0pVZNPX22PHYV5Km-vPTq2HtIqdboKyZy3Y4y3geOrRIFElZYoqjqSv5q9Jgtj5ERsNQIjefpyxW3EwPtFqMcDm4ebiAEpoEWRN4QYOMxnC9OUBeG9oLA0lTfmhgHLAtvJogJcYFzwngTsVo6HznsvPWy7UP3MINA"
+
+-- | Dummy client ID
+clientId :: Text
+clientId = "10d0e26a-e74e-4778-9d0b-5834de5a6956"
+
+-- | Dummy client secret
+clientSecret :: Text
+clientSecret = "aHR0cHM6Ly9naXRsYWIuY29tL2dhdmlua2ZsYW0vYXp1cmUtZXhwb3J0ZXI="
+
+-- | Dummy tenant ID
+tenantId :: Text
+tenantId = "ab2a7c40-80ea-4cd3-9ea1-10552c4df51d"
 
 -- | Dummy resource URI.
 resourceId :: Text
