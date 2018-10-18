08309cd8320a23812c2725441a109ee87808190d
Gavin Lam <me@gavin.hk>
2018-10-18 16:21:31 +0800

    Added headers strict ordering sorting mechanism for CSV

--- src/AzureExporter/Text/CSV.hs
+++ src/AzureExporter/Text/CSV.hs
@@ -12,7 +12,8 @@ import           Data.Text.Lazy (Text, intercalate, pack, unlines)
 
 import           Control.Lens ((^.))
 import qualified Data.HashMap.Strict as H
-import qualified Data.Set as Set
+import           Data.List (sort)
+import           Data.Set (Set, (\\), fromList, toList)
 import           Prelude hiding (unlines)
 
 import qualified AzureExporter.Data.CSV as C
@@ -22,9 +23,19 @@ import qualified AzureExporter.Data.Gauge as G
 renderCSV :: C.CSV -> Text
 renderCSV csv =
   unlines $ intercalate "," headers : map (renderRow headers) (csv ^. C.rows)
-    where headers = Set.toList (csv ^. C.headers)
+    where headers = sortHeaders (csv ^. C.headers)
 
 -- | Render a `CSV` row in `Text`.
 renderRow :: [Text] -> H.HashMap Text Text -> Text
 renderRow headers row =
   intercalate "," $ map (flip (H.lookupDefault "") row) headers
+
+-- |
+-- Sort headers in strict ordering.
+--
+-- series should go first, followed by value, timestamp and labels.
+sortHeaders :: Set Text -> [Text]
+sortHeaders hs =
+  coreHeaders ++ labelHeaders
+  where coreHeaders  = ["series", "value", "timestamp"]
+        labelHeaders = sort $ toList $ hs \\ fromList coreHeaders
