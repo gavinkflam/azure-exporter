344e6fafcc8f6447a3792af552a6d31746737556
Gavin Lam <me@gavin.hk>
2018-10-18 14:13:56 +0800

    Implemented CSV rendering mechanism

---
+++ src/AzureExporter/Text/CSV.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- |
+-- Utility to render a `CSV`.
+module AzureExporter.Text.CSV
+  (
+  -- * CSV
+    renderCSV
+  ) where
+
+import           Data.Text.Lazy (Text, intercalate, pack, unlines)
+
+import           Control.Lens ((^.))
+import qualified Data.HashMap.Strict as H
+import qualified Data.Set as Set
+import           Prelude hiding (unlines)
+
+import qualified AzureExporter.Data.CSV as C
+import qualified AzureExporter.Data.Gauge as G
+
+-- | Render a `CSV` in `Text`.
+renderCSV :: C.CSV -> Text
+renderCSV csv =
+  unlines $ intercalate "," headers : map (renderRow headers) (csv ^. C.rows)
+    where headers = Set.toList (csv ^. C.headers)
+
+-- | Render a `CSV` row in `Text`.
+renderRow :: [Text] -> H.HashMap Text Text -> Text
+renderRow headers row =
+  intercalate "," $ map renderVal $ map (flip (H.lookupDefault "") row) headers
+    where renderVal v = if v == "" then "\"\"" else v
