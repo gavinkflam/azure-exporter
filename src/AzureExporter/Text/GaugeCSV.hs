20b39a08cb78c45cac6dcd4698bc75ca840c37f6
Gavin Lam <me@gavin.hk>
2018-10-18 13:38:09 +0800

    Implemented mechanism deriving CSV from gauges

---
+++ src/AzureExporter/Text/GaugeCSV.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- |
+-- Utility to convert `Gauge`s to `CSV`.
+module AzureExporter.Text.GaugeCSV
+  (
+  -- * Gauge
+    toCSV
+  ) where
+
+import           Data.Text.Lazy (Text, pack)
+
+import           Control.Lens ((^.))
+import qualified Data.HashMap.Strict as H
+
+import qualified AzureExporter.Data.CSV as C
+import qualified AzureExporter.Data.Gauge as G
+
+-- | Convert `Gauge`s to `CSV`.
+toCSV :: [G.Gauge] -> C.CSV
+toCSV = foldl f C.empty
+  where f csv gauge = C.prependRow csv $ toRow gauge
+
+-- | Convert `Gauge` to a `CSV` row.
+toRow :: G.Gauge -> H.HashMap Text Text
+toRow g =
+  H.fromList $
+    [ ("series", g ^. G.name)
+    , ("value",  pack $ show (g ^. G.value))
+    ]
+    ++ map fLabel (g ^. G.labels)
+      where fLabel (k, v) = ("label_" <> k, v)
