c2eb680ef8b04172b1a63e6048e2c36ddb0766b7
Gavin Lam <me@gavin.hk>
2018-10-19 13:52:10 +0800

    Optimized gauge deriving mechanism for usages and added proper names sanitization

--- src/AzureExporter/Billing.hs
+++ src/AzureExporter/Billing.hs
@@ -9,7 +9,7 @@ module AzureExporter.Billing
   ) where
 
 import           Data.Char (toLower)
-import           Data.Maybe (catMaybes, fromMaybe)
+import           Data.Maybe (catMaybes, fromMaybe, isJust)
 import qualified Data.Text.Lazy as T
 
 import           Control.Lens ((^.))
@@ -26,6 +26,7 @@ import qualified Azure.Data.Billing.UsageAggregate as U
 import           Azure.Text.Scientific (showFixed)
 import           Azure.Text.Time (formatTime)
 import qualified AzureExporter.Data.Gauge as G
+import           AzureExporter.Util.Prometheus (metricName, sanitizeName, sanitizeLabelValue)
 import qualified AzureExporter.Data.ResourceMetadata as D
 import           AzureExporter.Util.Resource (parseResourceId)
 
@@ -43,21 +44,21 @@ gaugesFromUsageAggregate
   :: T.Text -> H.HashMap T.Text M.Meter -> U.UsageAggregate -> [G.Gauge]
 gaugesFromUsageAggregate currency meters usage =
   catMaybes
-  [ Just G.Gauge { G._name   = namePrefix <> "_usage"
-                 , G._help   = namePrefix <> "_usage"
+  [ Just G.Gauge { G._name   = name <> "_usage"
+                 , G._help   = name <> "_usage"
                  , G._labels = labels
                  , G._value  = quantity
                  , G._time   = Just endTime
                  }
   , costGauge <$> H.lookup (usage ^. U.properties ^. P.meterId) meters
   ]
-    where namePrefix  = gaugeName usage
+    where name        = gaugeName usage
           labels      = gaugeLabels usage
           quantity    = usage ^. U.properties ^. P.quantity
           endTime     = usage ^. U.properties ^. P.usageEndTime
           costGauge m =
-            G.Gauge { G._name   = namePrefix <> "_cost"
-                    , G._help   = namePrefix <> "_cost"
+            G.Gauge { G._name   = name <> "_cost"
+                    , G._help   = name <> "_cost"
                     , G._labels = labels ++ costGaugeLabels currency m
                     , G._value  = quantity * resolveUnitCost m
                     , G._time   = Just endTime
@@ -66,13 +67,14 @@ gaugesFromUsageAggregate currency meters usage =
 -- | Derive gauge name from `UsageAggregate`.
 gaugeName :: U.UsageAggregate -> T.Text
 gaugeName u =
-  "azure_" <> T.intercalate "_" (map sanitizeName segments)
-  where p        = u ^. U.properties
-        segments = catMaybes [ p ^. P.meterCategory, p ^. P.meterName ]
+  metricName $ "azure_" <> T.intercalate "_" ns
+  where p  = u ^. U.properties
+        ns = catMaybes [p ^. P.meterCategory, p ^. P.meterName, p ^. P.unit]
 
 -- | Derive gauge labels from `UsageAggregate`.
 gaugeLabels :: U.UsageAggregate -> [(T.Text, T.Text)]
 gaugeLabels u =
+  -- Static cloud provider label easing query composition
   [ ("cloud_provider",     "Azure")
   , ("meter_id",           p ^. P.meterId)
   , ("meter_category",     fromMaybe "Unknown" (p ^. P.meterCategory))
@@ -81,11 +83,12 @@ gaugeLabels u =
   , ("meter_region",       fromMaybe "N/A" (p ^. P.meterRegion))
   , ("meter_unit",         fromMaybe "Unknown" (p ^. P.unit))
   , ("subscription_id",    p ^. P.subscriptionId)
+  -- Time labels easing queries of aggregation
   , ("year",               T.pack $ formatTime "%Y" time)
   , ("month",              T.pack $ formatTime "%m" time)
   , ("year_month",         T.pack $ formatTime "%Y-%m" time)
   ]
-  ++ gaugeLabelsFromInstanceData (p ^. P.instanceData)
+  ++ maybe [] gaugeLabelsFromInstanceData (p ^. P.instanceData)
     where p    = u ^. U.properties
           time = p ^. P.usageEndTime
 
@@ -104,9 +107,8 @@ costGaugeLabels currency m =
   ]
 
 -- | Derive gauge labels from `InstanceData`.
-gaugeLabelsFromInstanceData :: Maybe I.InstanceData -> [(T.Text, T.Text)]
-gaugeLabelsFromInstanceData Nothing = []
-gaugeLabelsFromInstanceData (Just i) =
+gaugeLabelsFromInstanceData :: I.InstanceData -> [(T.Text, T.Text)]
+gaugeLabelsFromInstanceData i =
   [ ("resource_id",       T.toLower $ r ^. R.resourceUri)
   , ("resource_region",   r ^. R.location)
   , ("resource_group",    d ^. D.resourceGroup)
@@ -114,48 +116,26 @@ gaugeLabelsFromInstanceData (Just i) =
   , ("resource_provider", d ^. D.resourceProvider)
   , ("resource_type",     d ^. D.resourceType)
   ]
-  ++ gaugeLabelsFromResourceInfoMap "tag_"  (r ^. R.tags)
-  ++ gaugeLabelsFromResourceInfoMap "info_" (r ^. R.additionalInfo)
+  ++ maybe [] (gaugeLabelsFromResourceInfoMap "tag_")  (r ^. R.tags)
+  ++ maybe [] (gaugeLabelsFromResourceInfoMap "info_") (r ^. R.additionalInfo)
     where r = i ^. I.resourceData
           d = parseResourceId (r ^. R.resourceUri)
 
--- | Derive gauge labels from resource info map.
+-- |
+-- Derive gauge labels from resource info map.
+--
+-- Label name and value will be sanitized.
+-- Labels with empty or null value will be removed.
+--
+-- TODO: Remove labels with duplicated name.
 gaugeLabelsFromResourceInfoMap
-  :: T.Text -> Maybe (H.HashMap T.Text (Maybe T.Text)) -> [(T.Text, T.Text)]
-gaugeLabelsFromResourceInfoMap _ Nothing = []
-gaugeLabelsFromResourceInfoMap prefix (Just m) =
-  H.foldlWithKey' f [] m
-    where f ts k v = (prefix <> k, maybe "" sanitize v) : ts
-          sanitize = T.filter (not . flip elem illegalValueCharacters)
-
--- |
--- List of illegal characters to get rid of in the label values.
---
--- Tags and addition info values often contains new line characters for unknown
--- reasions
-illegalValueCharacters :: String
-illegalValueCharacters = "\r\n"
-
--- |
--- Sanitize and standardize gauge name.
---
--- The following steps will be done.
---
--- 1. Replace special characters with underscore.
--- 2. Downcase alphabetical characters.
--- 3. Remove consecutive underscores.
-sanitizeName :: T.Text -> T.Text
-sanitizeName n =
-  T.intercalate "_" $ filter (/= T.empty) $ T.splitOn "_" $ T.map f n
-    where f c = if c `elem` specialCharacters then '_' else toLower c
-
--- | List of special characters to get rid of in the gauge name.
-specialCharacters :: String
-specialCharacters = "-()/\\ "
+  :: T.Text -> H.HashMap T.Text (Maybe T.Text) -> [(T.Text, T.Text)]
+gaugeLabelsFromResourceInfoMap n m =
+  filter ((/="") . snd) $ H.foldlWithKey' f [] m
+    where f z k v = (n <> sanitizeName k, maybe "" sanitizeLabelValue v) : z
 
 -- |
 -- Construct a `Meter`s lookup `HashMap` indexed with meter ID for efficient
 -- searching.
 indexMeters :: [M.Meter] -> H.HashMap T.Text M.Meter
-indexMeters ms = H.fromList $ map item ms
-  where item m = (m ^. M.meterId, m)
+indexMeters = H.fromList . map (\m -> (m ^. M.meterId, m))
---
+++ src/AzureExporter/Util/Prometheus.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- |
+-- Utility functions related to Prometheus.
+module AzureExporter.Util.Prometheus
+  -- * Data Model
+  ( sanitizeName
+  , metricName
+  , sanitizeLabelValue
+  ) where
+
+import qualified Data.Text.Lazy as T
+
+-- |
+-- Sanitize metric and label name.
+--
+-- Retain only alphabetical characters and underscores.
+-- Illegal characters will be replaced with underscore.
+--
+-- The metric names and label names will prepend a static prefix afterwards.
+-- Thus we allow names starting with numeric characters.
+--
+-- <https://prometheus.io/docs/concepts/data_model/#metric-names-and-labels>
+sanitizeName :: T.Text -> T.Text
+sanitizeName = T.map (\c -> if isLegalNameChar c then c else '_')
+
+-- | Check if the character is a legal character for `sanitizeName`.
+isLegalNameChar :: Char -> Bool
+isLegalNameChar c
+  | 'a' <= c && c <= 'z' = True
+  | 'A' <= c && c <= 'Z' = True
+  | '0' <= c && c <= '9' = True
+  | c == '_'             = True
+  | otherwise            = False
+
+-- |
+-- Sanitize and standardize gauge name.
+--
+-- The metric name will be processed in three steps.
+--
+-- 1. Sanitized by `sanitizeName` first.
+-- 2. Remove consecutive underscore characters.
+-- 3. Downcase the alphabetical characters.
+metricName :: T.Text -> T.Text
+metricName =
+  T.toLower . T.intercalate "_" . filter (/= T.empty) . T.splitOn "_" . sanitizeName
+
+-- |
+-- Sanitize label values.
+--
+-- Tags and addition info values often contains new line characters for unknown
+-- reasons.
+sanitizeLabelValue :: T.Text -> T.Text
+sanitizeLabelValue = T.filter isLegalLabelValueChar
+
+-- | Check if the character is a legal character for `sanitizeLabelValue`.
+isLegalLabelValueChar :: Char -> Bool
+isLegalLabelValueChar c
+  | c == '\r' = False
+  | c == '\n' = False
+  | otherwise = True
