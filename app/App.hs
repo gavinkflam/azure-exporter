7e5b73f1b617a0a649c1355d462860eb8552acb2
Gavin Lam <me@gavin.hk>
2019-01-21 17:02:11 +0800

    Added route for fetching billing metrics

--- app/App.hs
+++ app/App.hs
@@ -12,12 +12,14 @@ import Control.Monad.App.AppM (AppM)
 import Network.Wai.Middleware.RequestLogger (logStdout)
 import Web.Scotty.Trans (ScottyT, get, middleware)
 
-import qualified Data.Route.Monitor as M
+import qualified Data.Route.Billing as Rob
+import qualified Data.Route.Monitor as Rom
 import qualified Data.Route.Ping as V
 
 -- | Scotty application for the exporter HTTP server.
 app :: ScottyT Text AppM ()
 app = do
     middleware logStdout
-    get "/monitor/metrics" M.metrics
+    get "/billing/metrics" Rob.metrics
+    get "/monitor/metrics" Rom.metrics
     get "/ping" V.ping
